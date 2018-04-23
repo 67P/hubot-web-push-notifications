d1feab1f53681218fe94b7f18d356359c644cf43
Sebastian Kippe <sebastian@kip.pe>
2018-04-23 13:03:13 +0200

    Add notification click event handler

--- demo/serviceworker.js
+++ demo/serviceworker.js
@@ -1,6 +1,12 @@
 self.addEventListener('push', function(event) {
-  debugger;
+  console.log('push event', event);
   event.waitUntil(self.registration.showNotification('ServiceWorker Cookbook', {
     body: 'Demo notification'
   }));
+});
+
+self.addEventListener("notificationclick", function(event) {
+  event.waitUntil(
+    clients.openWindow("http://localhost:8888/")
+  );
 });
